e43df96981f194c498bcbcc29303b67cc103490e
apricitine <apricitinecas@gmail.com>
2025-10-22 15:58:52 -0700

    lasjfk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
   import favicon from "$lib/assets/favicon.svg"
-	import { links } from "$lib/routes"
+  import { links } from "$lib/routes"
 
   let { children } = $props()
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+    rel="stylesheet"
+  />
   <style>
     body {
       margin: 0;
-    }
+			font-family: "Nunito Sans", sans-serif;
+    }	
   </style>
 </svelte:head>
 
